51384d9ff024a3d720fa575c8289b29025efecdb
elbonetsok <157169053+elbonetsok@users.noreply.github.com>
2024-12-09 12:08:53 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'your_secret_key'  # Voeg hier je eigen geheim sleutel toe
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:KlctEw4xeCGGCZNd@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Optioneel: voorkomt waarschuwingen
     WTF_CSRF_ENABLED = True
